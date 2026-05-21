29682698b64dcc43a2cee914babf3d03982f8874
myNameIsNewSpirit <rusnandapurnama@gmail.com>
2026-05-21 15:55:46 +0700

    updates

--- src/main/java/org/antibot/BotKiller.java
+++ src/main/java/org/antibot/BotKiller.java
@@ -2,25 +2,119 @@ package org.antibot;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.AsnResponse;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.model.CountryResponse;
+import com.maxmind.geoip2.record.Country;
+import org.antibot.Model.AsClassifiaction;
 import org.antibot.Model.Connections;
 import org.antibot.Model.ObjectTables.ObjAsClassfication;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class BotKiller {
-    static void main() {
+    File dbAsn;
+    File dbCity;
+    File dbCountry;
+    Connections database;
+
+    public BotKiller() throws SQLException {
+        this.dbAsn = new File("geolitedb/GeoLite2-ASN.mmdb");
+        this.dbCity = new File("geolitedb/GeoLite2-City.mmdb");
+        this.dbCountry = new File("geolitedb/GeoLite2-Country.mmdb");
+        this.database = new Connections();
+    }
+
+    static void main() throws SQLException, IOException {
+        BotKiller bot = new BotKiller();
+        CountryResponse rclassification = bot.getCountry("8.8.8.8");
+        System.out.println(rclassification.toJson());
+    }
+
+    public ObjAsClassfication romChecker(String net){
+        AsnResponse asnResponse = getAsnResponse(net);
+        return database.asClassifiaction.getByAsNumber("AS"+asnResponse.getAutonomousSystemNumber());
+    }
+
+    public String romCheckerJson(String net){
+        AsnResponse asnResponse = getAsnResponse(net);
+        ObjAsClassfication asClassifiaction = database.asClassifiaction.getByAsNumber("AS" + asnResponse.getAutonomousSystemNumber());
         try{
-            Connections db = new Connections();
-            ObjAsClassfication data = db.asClassifiaction.getByAsNumber("AS23693");
-            ObjectMapper maper = new ObjectMapper();
-            try{
-                String json = maper.writeValueAsString(data);
-                System.out.println(json);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (SQLException e) {
+            return objAsClassificationObjToJson(asClassifiaction);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+
+    public AsnResponse asnLookup(String net) {
+        return getAsnResponse(net);
+    }
+
+    public String asnLookupJson(String net) throws IOException {
+        return getAsnResponse(net).toJson();
+    }
+
+    public CityResponse cityLookup(String net){
+        return getCityResponse(net);
+    }
+
+    public String cityLookupJson(String net) throws IOException {
+        return getCityResponse(net).toJson();
+    }
+
+    public CountryResponse getCountry(String net){
+        return getCountryResponse(net);
+    }
+
+    public String getCountryJson(String net) throws IOException {
+        return getCountryResponse(net).toJson();
+    }
+
+    // internal use
+    private AsnResponse getAsnResponse(String net){
+        try(DatabaseReader reader = new DatabaseReader.Builder(dbAsn).build()){
+            InetAddress inet = InetAddress.getByName(net);
+            return reader.asn(inet);
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        } catch (GeoIp2Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private CityResponse getCityResponse(String net){
+        try(DatabaseReader reader = new DatabaseReader.Builder(dbCity).build()){
+            InetAddress inet = InetAddress.getByName(net);
+            return reader.city(inet);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (GeoIp2Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private CountryResponse getCountryResponse(String net){
+        try(DatabaseReader reader = new DatabaseReader.Builder(dbCountry).build()){
+            InetAddress inet = InetAddress.getByName(net);
+            return reader.country(inet);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (GeoIp2Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String objAsClassificationObjToJson(ObjAsClassfication object) throws JsonProcessingException{
+        ObjectMapper maper = new ObjectMapper();
+        return maper.writeValueAsString(object);
+    }
+
+
 }
